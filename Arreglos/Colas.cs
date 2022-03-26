25de82d2180206f959fa73a7574b22f83b46322c
Abraham Barragan Lira <bala.barragan3@hotmail.com>
2022-03-26 14:36:32 -0700

    agregando funciones de agregar, eliminar e imprimir

---
+++ Arreglos/Colas.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arreglos
+{
+    public class Colas
+    {
+        private string[] array;
+        private int max;
+        private int principio;
+        private int final;
+
+        public Colas(int tamano)
+        {
+            array = new string[tamano];
+            max = array.Length - 1;
+            principio = 0;
+            final = 0;
+        }
+
+        private bool ValidaVacio()
+        {
+            return ((principio < 1 && final < 1 ) 
+                || principio == final);
+        }
+
+        private bool ValidaLleno()
+        {
+            return (final > max);
+        }
+
+        public void Agregar(string dato)
+        {
+            if (ValidaLleno())
+            {
+                throw new Exception("Arreglo Lleno");
+            }
+            array[final] = dato;
+            final++;
+        }
+        public void Eliminar()
+        {
+            if (ValidaVacio())
+            {
+                throw new Exception("Arreglo Vacio");
+            }
+            array[principio] = null;
+            principio++;
+        }
+        public string Imprimir()
+        {
+            string datos = string.Empty;
+            if (ValidaVacio())
+            {
+                return "Arreglo Vacio";
+            }
+            for (int i = principio; i < final; i++)
+            {
+                if (i > principio)
+                {
+                    datos += "\n";
+                }
+                datos += $"[{i}] - {array[i]}";
+            }
+            return datos;
+        }
+    }
+}
---
+++ Arreglos/Pilas.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arreglos
+{
+    public class Pilas
+    {
+        private string[] array;
+        private int max;
+        private int tope;
+        public Pilas(int tamano)
+        {
+            array = new string[tamano];
+            max = array.Length - 1;
+            tope = 0;
+        }
+
+        private bool ValidaVacio()
+        {
+            //if (tope < 1)
+            //{
+            //    return true;
+            //}
+            //else
+            //{
+            //    return false;
+            //}
+
+            return (tope < 1);
+        }
+
+        private bool ValidaLleno()
+        {
+            return (tope > max);
+        }
+
+        public void Agregar (string dato)
+        {
+            if (ValidaLleno())
+            {
+                throw new Exception("Arreglo Lleno");
+            }
+
+            array[tope] = dato;
+            tope++;
+        }
+
+        public void Eliminar()
+        {
+            if (ValidaVacio())
+            {
+                throw new Exception("Arreglo Vacio");
+            }
+
+            tope--;
+            array[tope] = null;
+        }
+
+        public string Imprimir()
+        {
+            string datos = string.Empty;
+            if (ValidaVacio())
+            {
+                return "Arreglo Vacio";
+            }
+            for (int i = 0; i < tope; i++)
+            {
+                if(i > 0)
+                {
+                    datos += "\n";
+                }
+                datos += $"[{i}] - {array[i]}";
+            }
+            return datos;
+        }
+
+    }
+}
